26f4488d89e2121760d2c4edcf549652d6e4963d
varunraokadaparthi <varunraok123@gmail.com>
2022-05-17 23:09:35 +0530

    Spring boot initializer setup

---
+++ src/test/java/com/vehicle/springcloudproject/SpringcloudprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicle.springcloudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
